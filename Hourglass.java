912d210991a7008e0e13caeb2d5432695df9c4f9
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-11 19:19:10 +0000

    edited hourglass.java

--- Hourglass.java
+++ Hourglass.java
@@ -22,28 +22,47 @@ final class Hourglass {
   public static String topHalf(final int max, final int current) {
     String result = "";
     result += "\n";
-    if ()
-    for (int times = 0, times < current, times++) {
-      result += space;
-    }
-    if (current.equals(max)) {
+    if ((current + 1) == max) {
+      for (int times = 0; times < current; times++) {
+        result += space;
+      }
       result += star;
-      result += bottomHalf(max, 0);
+      result += bottomHalf(0, current, max);
     } else {
       final int row = max - current;
-      for (int layer = 0, layer < row, layer++) {
+      for (int times = 0; times < current; times++) {
+        result += space;
+      }
+      for (int layer = 0; layer < row; layer++) {
         result += star;
         result += space;
       }
       result += topHalf(max, current + 1);
     }
-    
+    return result;
   }
 
-  public static String bottomHalf(final int min, final int current) {
-    if (min.equals(current)) {
-      d
+  public static String bottomHalf(final int min, final int current,
+                                  final int value) {
+    String result = "";
+    result += "\n";
+    if ((current - 1) == min) {
+      for (int times = 0; times < current; times++) {
+        result += space;
+        result += star;
+      }
+    } else {
+      final int row = value - current;
+      for (int times = 0; times < current; times++) {
+        result += space;
+      }
+      for (int layer = 0; layer < row; layer++) {
+        result += star;
+        result += space;
+      }
+      result += bottomHalf(min, current - 1, value);
     }
+    return result;
   }
 
   /**
@@ -53,8 +72,21 @@ final class Hourglass {
   */
   public static void main(final String[] args) {
     final Scanner userInput = new Scanner(System.in);
-
-
-
-}
+    System.out.print("\nInput size of hourglass: ");
+    final String usrInput = userInput.nextLine();
+    try {
+      final int glassSize = Integer.valueOf(usrInput);
+      System.out.println();
+      if (glassSize > 0) {
+        String hourglass = topHalf(glassSize, 0);
+        System.out.print(hourglass);
+      } else {
+        System.out.println("\nPlease enter a positive integer.");
+      }
+    } catch (NumberFormatException errorCode) {
+      System.err.print("\nThis is not an integer.");
+    }
+    System.out.println("");
+    System.out.println("\nDone.");
+  }
 }
