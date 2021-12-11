583f2917245083a72f2288701cac52d189e74b3b
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-11 20:13:19 +0000

    edit

--- GlassBottom.java
+++ GlassBottom.java
@@ -3,9 +3,9 @@ public class GlassBottom {
   /**
   * The star string used for the hourglass visual.
   */
-  private static final String Star = "*";
+  private static final String STAR = "*";
 
-  private static final String Space = " ";
+  private static final String SPACE = " ";
 
   /**
   * Function creates the bottom half of the hourglass.
@@ -24,17 +24,17 @@ public class GlassBottom {
     }
     if (current == min) {
       for (int times = value; times > current; times--) {
-        result += Star;
-        result += Space;
+        result += STAR;
+        result += SPACE;
       }
     } else {
       final int row = value - current;
       for (int times = value; times > row; times--) {
-        result += Space;
+        result += SPACE;
       }
       for (int layer = value; layer > current; layer--) {
-        result += Star;
-        result += Space;
+        result += STAR;
+        result += SPACE;
       }
       result += bottomHalf(min, current - 1, value);
     }
